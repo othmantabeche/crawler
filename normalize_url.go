bdbaa47caab90b5b0e776ee7eed2a50009d36ec2
Othman Tabeche <otabeche23@gmail.com>
2026-05-12 16:19:49 +0200

    feat(normalizeUrl): add URL normalization and tests (#1)
    Implement `normalizeURL` to parse URLs, normalize case, and trim
    trailing slashes.
    Add table-driven tests covering scheme removal, lowercase handling,
    trailing slashes, and invalid input.

---
+++ normalize_url.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"errors"
+	"net/url"
+	"strings"
+)
+
+func normalizeURL(inputURL string) (string, error) {
+	parsedURL, err := url.Parse(inputURL)
+	if err != nil {
+		return "", errors.New("couldn't parse URL")
+	}
+
+	fullPath := parsedURL.Host + parsedURL.Path
+	fullPath = strings.ToLower(fullPath)
+
+	if fullPath[len(fullPath)-1] == '/' {
+		fullPath = fullPath[:len(fullPath)-1]
+	}
+
+	return fullPath, nil
+}
+
+/*
+	if url[:5] == "https" && url[len(url)-1] == '/' {
+		url := url[8 : len(url)-1]
+		return url, nil
+	} else if url[:4] == "http" && url[len(url)-1] == '/' {
+		url := url[7 : len(url)-1]
+		return url, nil
+	} else if url[:5] == "https" {
+		url := url[8:]
+		return url, nil
+	} else if url[:4] == "http" {
+		url := url[7:]
+		return url, nil
+	}
+*/
